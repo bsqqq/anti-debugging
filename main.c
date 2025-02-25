cb5a8cf52bb373275197cca701150283a6dfd783
Vinicius <viniciusmouraaragao@hotmail.com>
2025-02-25 10:40:28 -0300

    refatoração e atualização

--- main.c
+++ main.c
@@ -7,8 +7,6 @@ typedef struct _PEB {
   BYTE                          BeingDebugged;
   BYTE                          Reserved2[1];
   PVOID                         Reserved3[2];
-//  PPEB_LDR_DATA                 Ldr;
-//  PRTL_USER_PROCESS_PARAMETERS  ProcessParameters;
   PVOID                         Reserved4[3];
   PVOID                         AtlThunkSListPtr;
   PVOID                         Reserved5;
@@ -18,7 +16,6 @@ typedef struct _PEB {
   ULONG                         AtlThunkSListPtr32;
   PVOID                         Reserved9[45];
   BYTE                          Reserved10[96];
-//  PPS_POST_PROCESS_INIT_ROUTINE PostProcessInitRoutine;
   BYTE                          Reserved11[128];
   PVOID                         Reserved12[1];
   ULONG                         SessionId;
@@ -52,36 +49,10 @@ static void initialize_debug_scan() {
     if(peb->BeingDebugged == 1) {
         exit(1);
     };
-  //  if(IsDebuggerPresent()) {
-  //      printf("IsDebuggerPresent true\n");
-  //      exit(1);
-  //  }
 }
 
 int main() {
     
-  //  if(IsDebuggerPresent()) {
-  //      printf("Debugger detectado!");
-  //     exit(1);
-  //  }
-
-//  void initialize_debug_scan() {
-//       TEB* teb;
-
-//    __asm__ (
-//        "movl %%fs:0x18, %0;"   // Em sistemas de 32 bits, FS contém o offset do TEB na posição 0x18
-//        : "=r" (teb)             // O resultado vai para a variável teb
-//    );
-//    PEB* peb = (PEB*)teb->ProcessEnvironmentBlock;
-
-//    // Checando o campo BeingDebugged no PEB
-//    printf("BeingDebugged: %d\n", peb->BeingDebugged);
-//    if(peb->BeingDebugged == 1) {
-//        exit(1);
-//    };
-// }
-// initialize_debug_scan();
-
     char buffer[128];
     FILE *fp;
 
