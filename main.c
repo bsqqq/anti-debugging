8dd7d22ad138da5385284ec1d3000fbc1ea2267c
Vinicius <viniciusmouraaragao@hotmail.com>
2025-02-25 10:20:32 -0300

    IsDebuggerPreset() na main()

--- main.c
+++ main.c
@@ -39,19 +39,19 @@ typedef struct _TEB {
 
 // __attribute__((constructor))
 // static void initialize_debug_scan() {
-    //    TEB* teb;
+//        TEB* teb;
 
-    // __asm__ (
-    //     "movl %%fs:0x18, %0;"   // Em sistemas de 32 bits, FS contém o offset do TEB na posição 0x18
-    //     : "=r" (teb)             // O resultado vai para a variável teb
-    // );
-    // PEB* peb = (PEB*)teb->ProcessEnvironmentBlock;
+//     __asm__ (
+//         "movl %%fs:0x18, %0;"   // Em sistemas de 32 bits, FS contém o offset do TEB na posição 0x18
+//         : "=r" (teb)             // O resultado vai para a variável teb
+//     );
+//     PEB* peb = (PEB*)teb->ProcessEnvironmentBlock;
 
-    // // Checando o campo BeingDebugged no PEB
-    // //printf("BeingDebugged: %d\n", peb->BeingDebugged);
-    // if(peb->BeingDebugged == 1) {
-    //     exit(1);
-    // };
+//     // Checando o campo BeingDebugged no PEB
+//     //printf("BeingDebugged: %d\n", peb->BeingDebugged);
+//     if(peb->BeingDebugged == 1) {
+//         exit(1);
+//     };
   //  if(IsDebuggerPresent()) {
   //      printf("IsDebuggerPresent true\n");
   //      exit(1);
